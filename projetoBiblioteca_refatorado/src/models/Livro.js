c327d92416408feab0d3aa88179ab9152a6c8686
Gustavo  Barros <gustavoandrade0125@gmail.com>
2024-02-14 01:14:43 -0300

    validcoes_mongoose

--- projetoBiblioteca_refatorado/src/models/Livro.js
+++ projetoBiblioteca_refatorado/src/models/Livro.js
@@ -14,9 +14,21 @@ const livroSchema = new mongoose.Schema(
     },
     editora: {
       type: String, 
-      required: [true, "A editora é obrigatória"]
+      required: [true, "A editora é obrigatória"],
+      enum: {
+        values: ["Casa do código", "Alura", "Modernos"],
+        message: "A editora {VALUE} não é um valor permitido"
+      }
     },
-    numeroPaginas: {type: Number}
+    numeroPaginas: {
+      type: Number,
+      validate: {
+        validator: (valor) => { //min e max
+          return valor >=10 && valor<=5000;
+        },
+        message: "O número de páginas deve estar entre 10 e 5000. Valor Fornecido: {VALUE}"
+      }
+    }
   }
 );
 
